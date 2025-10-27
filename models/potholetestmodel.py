afeba3e34c507d79cbab5d8f7774e5b944febd21
esitachi <esitachi06@gmail.com>
2025-10-27 21:50:51 +0530

    Update potholetestmodel.py

--- models/potholetestmodel.py
+++ models/potholetestmodel.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
-# Load best trained model
+
 model = YOLO(r"C:\Users\eshaa\OneDrive\Desktop\pothole project\runs\detect\train\weights")
 
 # Test on a single image
 results = model.predict(r"Pothole.v1-raw.yolov8/train/images/img-647_jpg.rf.47741041990f3eb84cbc316d5d91642d.jpg", show=True)
 
-# OR test on multiple images
+# test on multiple images
 results = model.predict(source=r"C:\Users\eshaa\OneDrive\Desktop\pothole project\potholedataset\test\images", show=True)    
