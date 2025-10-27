1e1d1b4c8dbffa8164d27f331f2d811050da5705
esitachi <esitachi06@gmail.com>
2025-10-27 21:49:58 +0530

    Format code for better readability

--- models/pothole.py
+++ models/pothole.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # Load YOLOv8 pre-trained model
-model = YOLO("yolov8n.pt")  # you can also try yolov8s.pt for better accuracy
+model = YOLO("yolov8n.pt") 
 
 # Train the model
 model.train(
-    data=r"C:\Users\eshaa\OneDrive\Desktop\pothole project\pothole2.yaml",   # Dataset config
-    epochs=50,             # Training epochs
-    imgsz=640,             # Image size
-    batch=16               # Adjust based on GPU
+    data=r"C:\Users\eshaa\OneDrive\Desktop\pothole project\pothole2.yaml",  
+    epochs=50,             
+    imgsz=640,            
+    batch=16               
 )
